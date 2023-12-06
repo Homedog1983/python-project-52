fb2a8b189adeacd6e72b3660cb22f1e870687856
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-12-06 20:55:52 +0700

    just without IF...: STATIC_URL, STATIC_ROOT, STATICFILES_STORAGE

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
